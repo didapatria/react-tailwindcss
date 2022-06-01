4c15874664e2730b8f504111f774a9dbb7ca15c2
didapatria <didapatria3@gmail.com>
2022-06-01 20:37:05 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 function App() {
   return (
     <h1 className="flex justify-center items-center min-h-screen bg-slate-900 font-bold text-3xl text-center text-sky-600">
-      Tailwind CSS with Create React App
+      <div className="space-y-8">
+        <div className="flex justify-center animate-bounce">
+          <img src="./images/didapatria.png" alt="logo" className="rounded-full w-40 h-40" />
+        </div>
+        <div>Tailwind CSS with Create React App</div>
+      </div>
     </h1>
   );
 }
