ba0bbf5964b00bab12cc90dc8da9c218ad82189b
didapatria <didapatria3@gmail.com>
2022-06-01 20:02:05 +0700

    Update App.js

--- didapatria/src/App.js
+++ didapatria/src/App.js
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <h1 className="md:flex md:justify-center md:items-center md:min-h-screen bg-slate-900 text-3xl font-bold text-sky-600">
+    <h1 className="flex justify-center items-center min-h-screen bg-slate-900 font-bold text-3xl text-center text-sky-600">
       Tailwind CSS with Create React App
     </h1>
   );
