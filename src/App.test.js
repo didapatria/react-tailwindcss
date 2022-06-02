2f764df1d9c14e7c3e252405d1b309e5f91e371c
didapatria <didapatria3@gmail.com>
2022-06-02 14:22:45 +0700

    Test image and title

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders image', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const logo = screen.getByRole('img');
+  expect(logo).toHaveAttribute('src', './images/didapatria.png');
+  expect(logo).toHaveAttribute('alt', 'logo');
+});
+
+test('renders title', () => {
+  render(<App />);
+  const title = screen.getByText(/Tailwind CSS with Create React App/i);
+  expect(title).toBeInTheDocument();
 });
